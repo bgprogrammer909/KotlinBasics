78c982cd98152f42bf3c0d929688c72fb8736139
suchit <suchitbajracharya1@gmail.com>
2025-10-31 17:50:44 +0545

    start

---
+++ src/main/java/com/example/ai37a/day1.kt
@@ -0,0 +1,35 @@
+package com.example.ai37a
+
+import androidx.compose.animation.scaleOut
+
+fun main() {
+//    val name : String="suchit"
+//    val age:Int=20
+//    println("My name is ${name.uppercase()} and age is $age")
+//*************practice from below*****************
+
+// ARRAY BELOW ************************************************
+//    var age=arrayOf(11,23,34)
+//    age[0]=13
+//    print(age[0])
+
+//    var names=arrayOf("suchit","kunjang", "Samuel")
+//    names[0]="Suchit RB"
+//    print(names[0])
+
+//      var names=arrayListOf<String>("suchit","samuel","kunjang")
+//      names.add("shrijan")
+//      names.add(4,"abhikal")
+//      names.remove("suchit")
+//      names.removeAt(1)
+//      println(names)
+
+//      var mixedArraylist=arrayListOf<Any>("ME",1,3.0)
+//      println(mixedArraylist[0])
+//      println(mixedArraylist[1])
+//      println(mixedArraylist[2])
+
+// kotlin collection overview task below ************************
+
+
+}
